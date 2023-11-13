0d4beb780803262361d6d3d44b282ea958be32c4
ashik113 <ashikcsbtech@gmail.com>
2023-11-13 12:21:52 -0800

    Overflow error was fixed. and added SingleChildScrollView Widget for overcoming.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,241 +16,239 @@ class _HomeScreenState extends State<HomeScreen> {
       dropDownValue4,
       dropDownValue5;
 
-  String? result1 = '', result2 = '', result3 = '';
+  String? result1 = 'result1', result2 = 'result2', result3 = 'result3';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          padding: EdgeInsets.all(15.0),
+          child: Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text(
-                    'Name of the patient: ',
-                    style: kLabelTextStyle,
-                  ),
-                  SizedBox(
-                    width: 250.0,
-                    height: 50.0,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'User Name',
-                        hintText: 'Enter Your Name',
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Patient Name: ',
+                        style: kLabelTextStyle,
                       ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Symptom1: ',
-                    style: kSymptomTextStyle,
-                  ),
-                  DropdownButton<String>(
-                      value: dropDownValue1,
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropDownValue1 = newValue;
-                        });
-                      },
-                      items: symptoms
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList()),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Symptom2: ',
-                    style: kSymptomTextStyle,
-                  ),
-                  DropdownButton<String>(
-                      value: dropDownValue2,
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropDownValue2 = newValue;
-                        });
-                      },
-                      items: symptoms
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList()),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Symptom3: ',
-                    style: kSymptomTextStyle,
-                  ),
-                  DropdownButton<String>(
-                      value: dropDownValue3,
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropDownValue3 = newValue;
-                        });
-                      },
-                      items: symptoms
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList()),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Symptom4: ',
-                    style: kSymptomTextStyle,
-                  ),
-                  DropdownButton<String>(
-                      value: dropDownValue4,
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropDownValue4 = newValue;
-                        });
-                      },
-                      items: symptoms
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList()),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Symptom5: ',
-                    style: kSymptomTextStyle,
-                  ),
-                  DropdownButton<String>(
-                      value: dropDownValue5,
-                      onChanged: (newValue) {
-                        setState(() {
-                          dropDownValue5 = newValue;
-                        });
-                      },
-                      items: symptoms
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList()),
-                ],
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                    width: 250.0,
-                    child: MaterialButton(
-                      color: Colors.redAccent,
-                      onPressed: () {
-                        setState(() {
-                          result1 = 'predicted by decision tree';
-                        });
-                      },
-                      child: const Text(
-                        'Decision Tree',
-                        style: kButtonTextStyle,
+                      SizedBox(
+                        width: 180.0,
+                        height: 50.0,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'User Name',
+                            hintText: 'Enter Your Name',
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                  Text(
-                    '$result1',
-                    style: kLabelTextStyle,
+                  const SizedBox(
+                    height: 10.0,
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                    width: 250.0,
-                    child: MaterialButton(
-                      color: Colors.greenAccent,
-                      onPressed: () {
-                        setState(() {
-                          result2 = 'predicted by random forest';
-                        });
-                      },
-                      child: const Text(
-                        'Random Forest',
-                        style: kButtonTextStyle,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Symptom1: ',
+                        style: kSymptomTextStyle,
                       ),
-                    ),
+                      DropdownButton<String>(
+                          value: dropDownValue1,
+                          onChanged: (newValue) {
+                            setState(() {
+                              dropDownValue1 = newValue;
+                            });
+                          },
+                          items: symptoms
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()),
+                    ],
                   ),
-                  Text(
-                    '$result2',
-                    style: kLabelTextStyle,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 25.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    height: 50.0,
-                    width: 250.0,
-                    child: MaterialButton(
-                      color: Colors.blueAccent,
-                      onPressed: () {
-                        setState(() {
-                          result3 = 'predicted by Naive bayes';
-                        });
-                      },
-                      child: const Text(
-                        'Naive Bayes',
-                        style: kButtonTextStyle,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Symptom2: ',
+                        style: kSymptomTextStyle,
                       ),
-                    ),
+                      DropdownButton<String>(
+                          value: dropDownValue2,
+                          onChanged: (newValue) {
+                            setState(() {
+                              dropDownValue2 = newValue;
+                            });
+                          },
+                          items: symptoms
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()),
+                    ],
                   ),
-                  Text(
-                    '$result3',
-                    style: kLabelTextStyle,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Symptom3: ',
+                        style: kSymptomTextStyle,
+                      ),
+                      DropdownButton<String>(
+                          value: dropDownValue3,
+                          onChanged: (newValue) {
+                            setState(() {
+                              dropDownValue3 = newValue;
+                            });
+                          },
+                          items: symptoms
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Symptom4: ',
+                        style: kSymptomTextStyle,
+                      ),
+                      DropdownButton<String>(
+                          value: dropDownValue4,
+                          onChanged: (newValue) {
+                            setState(() {
+                              dropDownValue4 = newValue;
+                            });
+                          },
+                          items: symptoms
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const Text(
+                        'Symptom5: ',
+                        style: kSymptomTextStyle,
+                      ),
+                      DropdownButton<String>(
+                          value: dropDownValue5,
+                          onChanged: (newValue) {
+                            setState(() {
+                              dropDownValue5 = newValue;
+                            });
+                          },
+                          items: symptoms
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList()),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 50.0,
+                        width: 170.0,
+                        child: MaterialButton(
+                          color: Colors.redAccent,
+                          onPressed: () {
+                            setState(() {
+                              result1 = 'predicted';
+                            });
+                          },
+                          child: const Text(
+                            'Decision Tree',
+                            style: kButtonTextStyle,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '$result1',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 50.0,
+                        width: 170.0,
+                        child: MaterialButton(
+                          color: Colors.greenAccent,
+                          onPressed: () {
+                            setState(() {
+                              result2 = 'predicted';
+                            });
+                          },
+                          child: const Text(
+                            'Random Forest',
+                            style: kButtonTextStyle,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '$result2',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        height: 50.0,
+                        width: 170.0,
+                        child: MaterialButton(
+                          color: Colors.blueAccent,
+                          onPressed: () {
+                            setState(() {
+                              result3 = 'predicted';
+                            });
+                          },
+                          child: const Text(
+                            'Naive Bayes',
+                            style: kButtonTextStyle,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '$result3',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
